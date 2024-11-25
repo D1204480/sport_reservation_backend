14bf382fd40ce1bbdf46d92d05b27e054730ddfb
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-11-25 16:09:10 +0800

    create

---
+++ sport_place_reservation/src/main/java/com/example/sport_place_reservation/SportPlaceReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sport_place_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportPlaceReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportPlaceReservationApplication.class, args);
+	}
+
+}
---
+++ sport_place_reservation/src/test/java/com/example/sport_place_reservation/SportPlaceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sport_place_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportPlaceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
